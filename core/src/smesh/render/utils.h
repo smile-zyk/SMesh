fbc1e95b41dd0745ce49ecd276ec4e76a943ee03
Your Name <yongkangzhang123@foxmail.com>
2024-03-07 02:11:13 +0800

    The trackball camera implementation is complete

--- core/src/smesh/render/utils.h
+++ core/src/smesh/render/utils.h
@@ -1,11 +1,11 @@
 #pragma once
-#include "smesh/render/camera.h"
-#include <glm/glm.hpp>
+#include <glm/fwd.hpp>
 
 namespace smesh
 {
     namespace utils
     {
-        glm::vec4 NdcToWorld(glm::vec3 ndc_point, Camera* camera);
+        glm::vec4 NdcToWorld(glm::vec3 ndc_point, glm::mat4 view_matrix, glm::mat4 proj_matrix);
+        glm::vec2 Screen2Ndc(glm::vec2 screen_pos, glm::vec2 screen_size, bool is_reverse_y = true);
     }
 } // namespace smesh
